307c2f4a9f3e058accfc8284d45f52b7bf807884
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-11 21:21:30 +0300

    Update alertRoutes.js

--- src/routes/alertRoutes.js
+++ src/routes/alertRoutes.js
@@ -1,2 +1,11 @@
-router.get('/alerts', protect, alertController.getMyAlerts);
-router.put('/alerts/:id/read', protect, alertController.markAlertAsRead);
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/auth');
+const alertController = require('../controllers/alertController');
+
+router.use(protect);
+router.get('/', alertController.getMyAlerts);
+router.put('/:id/read', alertController.markAlertAsRead);
+router.put('/read-all', alertController.markAllAsRead);
+
+module.exports = router;
