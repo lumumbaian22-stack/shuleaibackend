fa620706894749e65ef8d05a5b36edb00df53d25
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-12 01:02:40 +0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
 const userController = require('../controllers/userController');
+const upload = require('../middleware/upload');
 
 // All user routes require authentication
 router.use(protect);
@@ -14,5 +15,6 @@ router.put('/preferences', userController.updatePreferences);
 router.get('/export', userController.exportMyData);
 router.post('/deactivate', userController.deactivateAccount);
 router.post('/profile-picture', userController.uploadProfilePicture);
+router.post('/profile-picture', upload.single('picture'), userController.uploadProfilePicture);
 
 module.exports = router;
