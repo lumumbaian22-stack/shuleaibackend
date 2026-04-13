a008e419a29b9893c836adf034c0698de950dd90
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-14 02:10:51 +0300

    Create subscriptionRoutes.js

---
+++ src/routes/subscriptionRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { protect, authorize } = require('../middleware/auth');
+const subscriptionController = require('../controllers/subscriptionController');
+
+router.get('/plans', subscriptionController.getPlans);
+router.get('/my-status', protect, authorize('parent'), subscriptionController.getMyStatus);
+router.post('/initiate-payment', protect, authorize('parent'), subscriptionController.initiatePayment);
+
+module.exports = router;
