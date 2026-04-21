774adfcf8d507bdb90a7b8c5cd544f01c34fb15a
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-21 17:39:00 +0300

    Update superAdminRoutes.js

--- src/routes/superAdminRoutes.js
+++ src/routes/superAdminRoutes.js
@@ -4,6 +4,8 @@ const { protect, authorize } = require('../middleware/auth');
 const superAdminController = require('../controllers/superAdminController');
 
 router.use(protect, authorize('super_admin'));
+// Add after existing routes
+router.get('/analytics', superAdminController.getSuperAdminAnalytics);
 
 // Dashboard and overview
 router.get('/overview', superAdminController.getOverview);
