1b38802f31b0150df9ee5a547c18f9cc0980a32d
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-28 00:34:38 +0300

    Update homeworkRoutes.js

--- src/routes/homeworkRoutes.js
+++ src/routes/homeworkRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
 const { createAssignment, getStudentAssignments, submitAssignment } = require('../controllers/homeworkController');
+const homeworkCtrl = require('../controllers/homeworkController');
 
 router.post('/assign', protect, authorize('teacher'), createAssignment);
 router.get('/student', protect, authorize('student'), getStudentAssignments);
 router.post('/submit/:assignmentId', protect, authorize('student'), submitAssignment);
+router.get('/teacher', protect, authorize('teacher'), homeworkCtrl.getTeacherAssignments);
 
 module.exports = router;
