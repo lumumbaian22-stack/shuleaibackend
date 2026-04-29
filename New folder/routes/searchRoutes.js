4096382fcde2499ade5569ea8fcee5b6440b1111
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-29 17:05:15 +0300

    Delete New folder directory

--- New folder/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const searchController = require('../controllers/searchController');
-
-router.get('/', protect, searchController.globalSearch);
-
-module.exports = router;
