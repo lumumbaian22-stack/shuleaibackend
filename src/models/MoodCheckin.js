366a14f68888aa6875b3b9434e9640c09e274b20
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-29 16:50:03 +0300

    Delete src/models directory

--- src/models/MoodCheckin.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const MoodCheckin = sequelize.define('MoodCheckin', {
-    userId: { type: DataTypes.INTEGER, allowNull: false },
-    mood: { type: DataTypes.STRING(20), allowNull: false },
-    note: DataTypes.TEXT
-  }, { timestamps: true });
-  return MoodCheckin;
-};
