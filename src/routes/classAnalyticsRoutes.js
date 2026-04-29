59f5367a426ccb840bc400aeec9edf368725d0df
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-04-29 16:50:19 +0300

    Delete src/routes directory

--- src/routes/classAnalyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, authorize } = require('../middleware/auth');
-const classAnalyticsController = require('../controllers/classAnalyticsController');
-
-router.get('/', protect, authorize('teacher'), classAnalyticsController.getClassAnalytics);
-
-module.exports = router;
--- src/routes/publicRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const publicController = require('../controllers/publicController');
-
-router.get('/duty/today', publicController.getPublicDutyView);
-router.get('/duty/week', publicController.getPublicWeeklyDuty);
-router.get('/school/:schoolId', publicController.getSchoolInfo);
-
-module.exports = router;
